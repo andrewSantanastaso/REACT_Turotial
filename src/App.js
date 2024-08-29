6cb42d81e8574b10331f5750efb01530684417b4
andrewSantanastaso <165174725+andrewSantanastaso@users.noreply.github.com>
2024-08-29 18:11:02 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Counter from "./Counter";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+// jsx <div></div>
---
+++ src/Button.js
@@ -0,0 +1,5 @@
+import "./button.css";
+
+export default function Button(props) {
+  return <button onClick={props.action}>{props.symbol}</button>;
+}
